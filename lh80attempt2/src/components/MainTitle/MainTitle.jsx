4e559b6f1fd92a3d676fab7bf632a1aaca11e23b
hrishi-sarma <sarmahrishi05@gmail.com>
2024-09-05 16:43:26 +0530

    updates

--- lh80attempt2/src/components/MainTitle/MainTitle.jsx
+++ lh80attempt2/src/components/MainTitle/MainTitle.jsx
@@ -1,36 +1,45 @@
 import React from 'react'
 import './MainTitle.css'
+import Pfps from './Pfps.jpeg';
 
 const MainTitle = () => {
   return (
     <div className='Main'>
-        <div className='Title'>
-            <h1>
-                Hiksdjfld
-            </h1>
-            <h1>
-                dgfhdsf
-            </h1>
+        <div className="titlestuff">
+            <div className='Title'>
+                <h1>
+                    Hiksdjfld
+                </h1>
+                <h1>
+                    dgfhdsf
+                </h1>
 
-            <div class="container">
-                    <div class="typed-out" id="typed-text"></div>
-            </div>
-            
-        </div>
-
-        <div className="Body">
-            <div className='sec1'>
-                sdhds
-            </div>
+                <div class="container">
+                        <div class="typed-out" id="typed-text"></div>
+                </div>
                 
-            <div className='sec1'>
-                ijfisdf
             </div>
 
-            <div className='sec1'>
-                oijfidsjf
+            <div className="titleImg">
+                <img src={Pfps} alt="no image" className="profimg" />
             </div>
+
         </div>
+
+        <div className="Bar"/>
+
+        <div className="aboutme">
+            <h1>
+                About Me : 
+            </h1>
+
+            <p>
+                description texts idk what to put here rn
+            </p>
+        </div>
+
+
+        
 
     </div>
 
