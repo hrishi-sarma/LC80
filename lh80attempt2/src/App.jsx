5521ae6148d93c412ce20e26530b95930249b7ac
hrishi-sarma <sarmahrishi05@gmail.com>
2024-09-08 21:26:19 +0530

    added skills

--- lh80attempt2/src/App.jsx
+++ lh80attempt2/src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/Nav/navbar'
 import MainContent from './components/MainTitle/MainTitle.jsx'
 import { Experience } from './components/Experience.jsx'
 import ProjectContent from './components/projects/projects.jsx'
+import SkillsContent from './components/myskills/skills.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <MainContent/>
+      <SkillsContent/>
       <ProjectContent/>
     </div> 
   )
---
+++ lh80attempt2/src/components/myskills/skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const skills = () => {
+  return (
+    <div className='Main'>
+        <h1>
+            yo
+        </h1>
+
+    </div>
+
+    
+  )
+}
+
+export default skills
